2301f89b9053569967c79c5fc84ce856d30c7778
utkualtintas <66638628+utkualtintas@users.noreply.github.com>
2020-06-08 20:54:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'download.jpeg') {
+      myImage.setAttribute ('src','deniz.jpeg');
+    } else {
+      myImage.setAttribute ('src','download.jpeg');
+    }
+} 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h4');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
